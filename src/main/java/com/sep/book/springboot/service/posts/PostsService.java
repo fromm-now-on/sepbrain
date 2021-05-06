32f7288d1f3cf49746be80ac20c49d21558d57cf
fromm-now-on <iamsungeun@gmail.com>
2021-05-06 15:01:26 +0900

    ~110p

---
+++ src/main/java/com/sep/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.sep.book.springboot.service.posts;
+
+
+import com.sep.book.springboot.domain.posts.PostsRepository;
+import com.sep.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
